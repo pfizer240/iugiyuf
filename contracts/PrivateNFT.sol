dfab2bc5e5a82ce9602898c771c1837726c0a975
pfizer240 <pfizer240@gmail.com>
2024-08-26 18:10:36 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("ut","uy")
+        ERC721("uty","tr")
         Ownable(initialOwner)
     {}
 
